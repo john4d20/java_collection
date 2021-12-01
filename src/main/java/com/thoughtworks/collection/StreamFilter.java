84081fa03012614de76d62dda08ba6c64c6c9556
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-01 12:00:23 +0800

    getCommonElements and change EndWithS

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -10,12 +10,12 @@ public class StreamFilter {
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        return words.stream().filter(word -> word.substring(word.length() - 1).equals("s")).collect(Collectors.toList());
+        return words.stream().filter(word -> word.endsWith("s")).collect(Collectors.toList());
 
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
-        return null;
+        return numbers.stream().filter(number -> anotherNumbers.contains(number)).collect(Collectors.toList());
     }
 
 
